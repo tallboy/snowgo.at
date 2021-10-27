3b7048f04b9fb7a603fb664e9c49b0b3cb596d08
Tommy Ryan <tommy@thetallboy.com>
2021-10-27 12:43:44 -0700

    (goat) Chase Murphy and some clean up (#32)
    * (goat) Chase Murphy and some clean up
    
    * (lint) lint
    
    Co-authored-by: tommy.ryan <tommy.ryan@servicenow.com>

--- js/snowgoat.js
+++ js/snowgoat.js
@@ -41,7 +41,8 @@
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  bootstrap.ScrollSpy(document.body, {
+  /* eslint-disable no-new */
+  new bootstrap.ScrollSpy(document.body, {
     target: '#mainNav',
     offset: 80,
   });
